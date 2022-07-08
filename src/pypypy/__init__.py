ee7db3a6235ca8f94a4c9e8e73f0b47b2d84b799
Damian Cyntler <damian@cyntler.com>
2022-07-08 11:35:45 +0200

    Add hello print

--- src/pypypy/__init__.py
+++ src/pypypy/__init__.py
@@ -1,11 +1,1 @@
-__version__ = "0.1.0"
-
-xd = []
-
-
-def test():
-    print("qweqwe")
-
-
-def test2():
-    print("qweqwe")
+from pypypy import *
---
+++ src/pypypy/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("hello from main")
+
+
+if __name__ == "__main__":
+    main()
